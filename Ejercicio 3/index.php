238971f17521d9053c8a43eb73f69d60f95d4996
Jaime Navarrete <jaimenava123@outlook.com>
2020-06-15 17:41:00 -0600

    Ejercicio 3 - Completado (sin css) | Ejercicio 4 - Iniciado

---
+++ Ejercicio 3/index.php
@@ -0,0 +1,54 @@
+<?php
+
+if(isset($_GET['alumnos'])) {
+    $alumnos = $_GET['alumnos'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <?php if(isset($_GET['alumnos'])) : ?>
+        <h1>Evaluar alumnos</h1>
+        <form action="resultados.php" method="post">
+            <table>
+                <tr>
+                    <th>Carnet</th>
+                    <th>Nombre</th>
+                    <th>Nota 1</th>
+                    <th>Nota 2</th>
+                    <th>Nota 3</th>
+                </tr>
+            </table>
+            <?php            
+                for($i = 1; $i<=$alumnos; $i++){
+                    echo "<div class='datos_alumno'>";
+                    echo "<span>$i.- </span>";
+                    echo "<input type='hidden' name='alumnos' value='$alumnos'>";
+                    echo "<input type='text' name='" . $i . "carnet' placeholder='Carnet:'>";
+                    echo "<input type='text' name='" . $i . "nombre' placeholder='Nombre completo:'>";
+                    echo "<input type='text' name='" . $i . "nota1' placeholder='Nota 1:'>";
+                    echo "<input type='text' name='" . $i . "nota2' placeholder='Nota 2:'>";
+                    echo "<input type='text' name='" . $i . "nota3' placeholder='Nota 3:'>";
+                    echo "</div>";
+                }            
+            ?>          
+            <input type="submit" name="Enviar">
+        </form>
+    <?php else : ?>
+        <h1>Número de alumnos a evaluar</h1>
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
+            <input type="number" name="alumnos" placeholder="Número de alumnos:">
+            <input type="submit" value="Enviar">
+        </form>
+    <?php endif ?>
+</body>
+</html>
---
+++ Ejercicio 3/resultados.php
@@ -0,0 +1,93 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $alumnos = (int)$_POST['alumnos'];
+    $aprobados = 0;
+    $reprobados = 0;
+    $posMax = 0;
+    $max = 0;
+    $posMin = 0;
+    $min = 10;
+
+    for($i = 1; $i<=$alumnos; $i++){
+        $carnet[$i] = $_POST[$i . 'carnet'];
+        $nombre[$i] = $_POST[$i . 'nombre'];
+        $nota1[$i] = $_POST[$i . 'nota1'];
+        $nota2[$i] = $_POST[$i . 'nota2'];
+        $nota3[$i] = $_POST[$i . 'nota3'];
+
+        $promedio[$i] = round(($nota1[$i] + $nota2[$i] + $nota3[$i]) / 3, 2);
+        
+        if($promedio[$i] >= 6.5) {
+            $estado[$i] = "Aprobado";
+            $aprobados++;
+        }
+        else {
+            $estado[$i] = "Reprobado";
+            $reprobados++;
+        }
+
+        $porcentajeAprobados = round($aprobados / $alumnos * 100, 2);
+        $porcentajeReprobados = round(100 - $porcentajeAprobados, 2);
+
+        if($promedio[$i] > $max) {
+            $posMax = $i;
+            $max = $promedio[$i];
+        }
+        if($promedio[$i] < $min) {
+            $posMin = $i;
+            $min = $promedio[$i];
+        }
+    }
+}
+else {
+    header('Location: index.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Notas de alumnos</title>
+</head>
+<body>
+    <h1>Resultados del grupo de alumnos</h1>
+    <table>
+        <tr>
+            <th>Carnet:</th>
+            <th>Nombre:</th>
+            <th>Nota1:</th>
+            <th>Nota2:</th>
+            <th>Nota3:</th>
+            <th>Promedio:</th>
+        </tr>
+
+        <?php    
+            for($i = 1; $i<=$alumnos; $i++){
+                echo "<tr>";
+                echo "<td>$carnet[$i]</td>";
+                echo "<td>$nombre[$i]</td>";
+                echo "<td>$nota1[$i]</td>";
+                echo "<td>$nota2[$i]</td>";
+                echo "<td>$nota3[$i]</td>";
+                echo "<td>$promedio[$i]</td>";
+                echo "<td>$estado[$i]</td>";
+                echo "</tr>";
+            }       
+        ?>
+
+    </table>
+
+    <div class="wrapper">
+        <p>Porcentaje de alumnos aprobados: <?php echo $porcentajeAprobados?>%</p>
+        <p>Porcentaje de alumnos reprobados: <?php echo $porcentajeReprobados?>%</p>
+        <p>El alumno con mayor promedio: <?php echo strtoupper($nombre[$posMax]) . ' con promedio de ' . $promedio[$posMax];?></p>
+        <p>El alumno con menor promedio: <?php echo strtoupper($nombre[$posMin]) . ' con promedio de ' . $promedio[$posMin];?></p>
+    </div>
+</body>
+</html>
